d03a4f66ce1ef1fd5893ce0b4dbeac5ac458789d
+ <+>
2023-01-22 22:43:11 -0500

    commit tree 269d92bf4c8b2bf4aa5caf0c03bc2c1c9de97ef2

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Add your code here
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({ name, email })
+    })
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error('Error submitting data');
+        })
+        .then(data => {
+            if (data.id) {
+                let result = document.getElementById("result");
+                if (!result) {
+                    result = document.createElement("UL");
+                    result.id = "result";
+                    document.body.appendChild(result);
+                }
+                let node = document.createElement("LI");
+                let textnode = document.createTextNode(data.id);
+                node.appendChild(textnode);
+                result.appendChild(node);
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            let errorMessage = document.createElement('p');
+            errorMessage.textContent = error.message;
+            errorMessage.style.color = 'red';
+            document.body.appendChild(errorMessage);
+        });
+}
